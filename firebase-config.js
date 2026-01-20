9475165d051a0f528f7e35ca3d32e8694d72969c
Nicolae <nicolae.iurcu@colegiu.utm.md>
2026-01-20 21:38:49 +0200

    v0.1

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+// Replace with your Firebase project configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAgotVJxT1tGfojqJh9A3JLxG2QdxMie5w",
+
+  authDomain: "matharena-f2c5b.firebaseapp.com",
+
+  projectId: "matharena-f2c5b",
+
+  storageBucket: "matharena-f2c5b.firebasestorage.app",
+
+  messagingSenderId: "1087998817293",
+
+  appId: "1:1087998817293:web:63292448ccb79935a81144",
+
+  
+
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
